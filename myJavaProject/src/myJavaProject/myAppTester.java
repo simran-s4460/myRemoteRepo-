9bdee6ca83f00cd0391aadde969ec540118949cc
simransimran <simrangill2602@gmail.com>
2020-05-22 02:42:08 -0700

    program gives correct result

---
+++ myJavaProject/src/myJavaProject/myAppTester.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package myJavaProject;
+
+/**
+ * @author simran
+ *
+ */
+public class myAppTester {
+
+	/**
+	 * @param args
+	 */
+	static int a=5;
+	static int b=10;
+	static int c=0;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Software Engineering World");
+		c=a+b;
+		System.out.println("Sum of two numbers: "+c);
+	
+
+	}
+
+}
